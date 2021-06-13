bf0a087d329464c989ec3d2cd888c21bb453bedb
josephmartin721 <josephmartin721@yahoo.com>
2021-06-13 16:59:33 -0500

    restyling filing closer to class expectations

---
+++ src/components/table.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const Table = () => {
+    return (
+        <div>
+            Home Page Example
+
+            --home page table
+        </div>
+    )
+}
+
+export default Table;
