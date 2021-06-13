9d4b654194594625b83e699e5c8164c09f6a63ce
josephmartin721 <josephmartin721@yahoo.com>
2021-06-13 17:46:55 -0500

    added create-react-app

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import Hom from './pages/Home';
+import Logo from './logo.svg';
+import Home from './pages/Home';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
+      <Logo/>
+      
       <Home/>
     </div>
   );
--- src/components/table.js
+++ src/components/table.js
@@ -3,7 +3,7 @@ import React from 'react'
 const Table = (props) => {
     return (
         <div>
-            --home page table
+            {JSON.stringigy(props.employeeList)}
         </div>
     )
 }
