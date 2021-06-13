51b09560996ed2f03bb6786919af679300537a51
josephmartin721 <josephmartin721@yahoo.com>
2021-06-13 18:47:45 -0500

    new build and deploy scripts run

--- src/App.js
+++ src/App.js
@@ -1,5 +1,4 @@
 import React from "react";
-import Logo from './logo.svg';
 import Home from './pages/Home';
 import Table from './components/Table';
 import Jumbotron from "./components/Jumbotron";
@@ -9,7 +8,6 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <Logo/>
       <Home/>
       <Jumbotron/>
       <Table/>
