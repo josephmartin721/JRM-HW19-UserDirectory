7878e8a34f04796399213c6434d9e583e0d3b34c
josephmartin721 <josephmartin721@yahoo.com>
2021-06-13 19:00:15 -0500

    component edits

--- src/components/Jumbotron.js
+++ src/components/Jumbotron.js
@@ -4,7 +4,7 @@ const Jumbotron = () => {
   return (
     <div className="jumbotron text-center">
       <h1 className="display-4">Employee Directory</h1>
-      <p className="lead">This is an employee directory created with React.</p>
+      <p className="lead">This employee directory was created using React.</p>
       <hr className="my-4" />
       <p>
         An employee or manager benefits greatly from being able to view
