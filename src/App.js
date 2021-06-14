87111f0a1e6b77cee7aa4af33f342f750f024090
josephmartin721 <josephmartin721@yahoo.com>
2021-06-13 21:02:12 -0500

    near final product

--- src/App.js
+++ src/App.js
@@ -1,7 +1,5 @@
 import React from "react";
 import Home from './pages/Home';
-import Table from './components/Table';
-import Jumbotron from "./components/Jumbotron";
 import './App.css';
 
 
@@ -9,8 +7,6 @@ function App() {
   return (
     <div className="App">
       <Home/>
-      <Jumbotron/>
-      <Table/>
     </div>
   );
 }
